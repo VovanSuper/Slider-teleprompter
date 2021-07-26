493183e4733c83cae8e40ec22349676ded74e3a1
Vovan_Super <ovsyukov@yandex.com>
2021-07-26 21:41:14 +0300

    ref: save files using writable Streams, save fileName to metadata

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,61 +17,112 @@ export default function () {
   optToSliderButtons();
   handleRecording();
   handleDownloadClick();
+  navigator.permissions.query({ name: 'microphone' }).then((micAllowed) => micAllowed.state === 'granted');
   fromStore.subscribe(({ clips, recording }) => {
     getStatusBoxEl().innerHTML = !!recording ? `<p>Recording</P>` : '<small style="color: #ccc; font-size: small;">Click `R` to record</small>';
     renderClips({ clips }, rootEl);
     getDownloadBtn().style.opacity = !!clips?.length ? 1 : 0;
-    if (recording) getDownloadBtn().classList.add('btn-recording')
-    else getDownloadBtn().classList.remove('btn-recording')
+    if (recording) getDownloadBtn().classList.add('btn-recording');
+    else getDownloadBtn().classList.remove('btn-recording');
   });
 }
 
 function handleDownloadClick() {
-  getDownloadBtn().addEventListener('click', function downloadHandler(e) {
+  getDownloadBtn().addEventListener('click', async function downloadHandler(e) {
     const { clips, recording } = fromStore.getStateSnapshot();
     if (recording) return;
-    // const allRecs = clips.map(({ noteId, data }, i) => ({
-    //   file: new File([data], `Slide-${noteId}-${i}`, { type: 'audio/webm' }),
-    // }));
-    let meta = { clips: [] };
+    const type = 'audio/webm';
+    const allRecs = clips.map(({ id, slides, data }) => ({
+      file: new File([data], `Slide-${id}`, { type }),
+      id,
+      slides,
+    }));
 
-    clips.forEach(({ id, slides, data }) => {
-      let reader = new FileReader();
-      reader.readAsDataURL(data);
-      reader.onloadend = (_e) => {
-        const audioBas64 = reader.result.toString();
+    let meta = { clips: [] };
+    const audiofileOpts = {
+      types: [
+        {
+          description: 'Recorded clips',
+          accept: { [type]: ['.weba'] },
+        },
+      ],
+    };
+    const metadataOpts = {
+      types: [
+        {
+          description: 'Recorded clips metadata',
+          accept: { 'text/json': ['.json'] },
+        },
+      ],
+    };
+
+    await Promise.all(
+      allRecs.map(async ({ file, slides, id }) => {
+        let fileHandle = await window.showSaveFilePicker(audiofileOpts);
+
+        const writable = await fileHandle.createWritable();
+
+        await writable.write(file, fileHandle.name, file.type);
+        await writable.close();
+
         meta = {
           clips: meta.clips.concat({
             id,
             slides,
-            audio: audioBas64,
+            // audio: audioBas64,
+            file: `${file.name}.weba`,
           }),
         };
-        const aEl = document.createElement('a');
-        aEl.href = window.URL.createObjectURL(data);
+      })
+    )
+      .then(async () => {
+        let fileHandle = await window.showSaveFilePicker(metadataOpts);
 
-        aEl.download = `Clip-${id}.webm`;
-        aEl.style.display = 'none';
-        document.body.appendChild(aEl);
+        const writable = await fileHandle.createWritable();
+        await writable.write(JSON.stringify(meta), fileHandle.name + '.json' || 'clips-metadata.json');
+        await writable.close();
+      })
+      .catch((e) => {
+        console.error('Error writing files : ', e);
+      });
 
-        aEl.click();
+    // clips.forEach(({ id, slides, data }) => {
+    //   let reader = new FileReader();
+    //   reader.readAsDataURL(data);
+    //   reader.onloadend = (_e) => {
+    //     const audioBas64 = reader.result.toString();
+    //     meta = {
+    //       clips: meta.clips.concat({
+    //         id,
+    //         slides,
+    //         audio: audioBas64,
+    //       }),
+    //     };
+    //     const aEl = document.createElement('a');
+    //     aEl.href = window.URL.createObjectURL(data);
 
-        document.body.removeChild(aEl);
-      };
-    });
+    //     aEl.download = `Clip-${id}.webm`;
+    //     aEl.style.display = 'none';
+    //     document.body.appendChild(aEl);
 
-    setTimeout(() => {
-      const aEl = document.createElement('a');
-      const filename = `media-metadata.json`;
-      aEl.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(meta)));
-      aEl.setAttribute('download', filename);
+    //     aEl.click();
 
-      aEl.style.display = 'none';
-      document.body.appendChild(aEl);
+    //     document.body.removeChild(aEl);
+    //   };
+    // });
 
-      aEl.click();
+    // setTimeout(() => {
+    //   const aEl = document.createElement('a');
+    //   const filename = `media-metadata.json`;
+    //   aEl.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(meta)));
+    //   aEl.setAttribute('download', filename);
 
-      document.body.removeChild(aEl);
-    }, 100);
+    //   aEl.style.display = 'none';
+    //   document.body.appendChild(aEl);
+
+    //   aEl.click();
+
+    //   document.body.removeChild(aEl);
+    // }, 100);
   });
 }
