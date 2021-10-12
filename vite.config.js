042f29550b426b6d294f55d32be3a317679881dc
Vovan_Super <ovsyukov@yandex.com>
2021-10-12 20:38:04 +0300

    fix(ui:min): serve logo properly

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { join } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: join(__dirname, 'src'),
-  build: {
-    emptyOutDir: true,
-    outDir: join(__dirname, 'dist'),
-    assetsDir: 'public',
-    manifest: true,
-    minify: 'esbuild',
-    polyfillDynamicImport: true,
-    assetsInlineLimit: 4194304,
-    sourcemap: 'inline',
-    target: ['es2015', 'es2020'],
-    write: true,
-    cssCodeSplit: true,
-  },
+	root: join(__dirname, 'src'),
+	build: {
+		emptyOutDir: true,
+		outDir: join(__dirname, 'dist'),
+		assetsDir: 'public',
+		manifest: true,
+		minify: 'esbuild',
+		polyfillDynamicImport: true,
+		assetsInlineLimit: 4194304,
+		sourcemap: 'inline',
+		target: ['es2015', 'es2020'],
+		write: true,
+		cssCodeSplit: true,
+	},
 });
